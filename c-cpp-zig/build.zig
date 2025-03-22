f27964527f7c7f014b8d066daeb05a5aba65b978
Carl Åstholm <carl@astholm.se>
2025-03-22 22:35:34 +0100

    Update the C/C++/Zig example

--- c-cpp-zig/build.zig
+++ c-cpp-zig/build.zig
@@ -12,66 +12,57 @@ pub fn build(b: *std.Build) void {
         zig,
     }, "main", "Which language's main function to use (default: Zig)") orelse .zig;
 
-    const c_flags = .{
-        "-Wall",
-        "-Werror",
-    };
+    const c_flags = .{ "-Wall", "-Werror" };
 
-    const exe_mod = b.createModule(.{
+    const app_mod = b.createModule(.{
         .root_source_file = if (main_language == .zig) b.path("main.zig") else null,
         .target = target,
         .optimize = optimize,
         .link_libc = true,
         .link_libcpp = true,
     });
-    const exe = b.addExecutable(.{
+    const app = b.addExecutable(.{
         .name = "greeter",
-        .root_module = exe_mod,
+        .root_module = app_mod,
     });
 
     // Make sure we can find 'greet.h'.
-    exe_mod.addIncludePath(b.path("."));
+    app_mod.addIncludePath(b.path("."));
 
     // Add the file containing the main entry point.
     switch (main_language) {
-        .c => exe_mod.addCSourceFiles(.{
-            .files = &.{
-                "main.c",
-            },
+        .c => app_mod.addCSourceFile(.{
+            .file = b.path("main.c"),
             .flags = &c_flags,
         }),
-        .cpp => exe_mod.addCSourceFiles(.{
-            .files = &.{
-                "main.cpp",
-            },
+        .cpp => app_mod.addCSourceFile(.{
+            .file = b.path("main.cpp"),
             .flags = &c_flags,
         }),
-        .zig => {}, // Already handled in 'addExecutable'
+        .zig => {}, // Already handled by 'b.createModule()'
     }
 
-    // Add the greet implementations.
-    exe_mod.addCSourceFiles(.{
-        .files = &.{
-            "greet.c",
-            "greet.cpp",
-        },
+    // Add the greeter implementations.
+    app_mod.addCSourceFiles(.{
+        .files = &.{ "greet.c", "greet.cpp" },
         .flags = &c_flags,
     });
     // The Zig compilation model only supports compiling one root Zig source file, which when
     // compiling executables is expected to contain the main entry point. If we want to compile
-    // 'greet.zig' like we would C code we need to explicitly compile it as a separate object.
-    exe_mod.addObject(b.addObject(.{
+    // 'greet.zig' like we would C code, we need to explicitly compile it into a separate object.
+    app_mod.addObject(b.addObject(.{
         .name = "greet",
         .root_source_file = b.path("greet.zig"),
         .target = target,
         .optimize = optimize,
     }));
 
-    b.installArtifact(exe);
+    b.installArtifact(app);
 
-    const run_exe = b.addRunArtifact(exe);
-    run_exe.step.dependOn(b.getInstallStep());
+    const run_app = b.addRunArtifact(app);
+    if (b.args) |args| run_app.addArgs(args);
+    run_app.step.dependOn(b.getInstallStep());
 
     const run = b.step("run", "Run the app");
-    run.dependOn(&run_exe.step);
+    run.dependOn(&run_app.step);
 }
